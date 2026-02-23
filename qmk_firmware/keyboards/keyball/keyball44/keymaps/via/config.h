7417c05ee107dcabd71e65d31cfc9b25b2031d94
keiko-yellow <freetoyoyu@gmail.com>
2026-02-23 11:37:11 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -40,5 +40,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000
-#define COMBO_COUNT 7
-#define COMBO_TERM 60
+#define COMBO_COUNT 8
+#define COMBO_TERM 80
