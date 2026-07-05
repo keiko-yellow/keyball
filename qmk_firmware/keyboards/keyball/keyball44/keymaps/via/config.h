ac0ecd4bf53b7a780d1e5a4c193a86fbf5bcb3fe
keiko-yellow <freetoyoyu@gmail.com>
2026-07-05 23:54:05 +0900

    Update dynamic keymap layer count and combo settings
    Increased the dynamic keymap layer count from 5 to 7 and added a configuration for combo keys to only be recognized from layer 0.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -8,19 +8,11 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
 #ifdef RGBLIGHT_ENABLE
 //#    define RGBLIGHT_EFFECT_BREATHING
@@ -41,5 +33,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000
 #define AUTO_MOUSE_THRESHOLD 30
+
 #define COMBO_COUNT 17
 #define COMBO_TERM 80
+
+/*
+ * Auto Mouseでレイヤー1が有効になっても、
+ * Combo判定だけはレイヤー0のキーコードを基準にする。
+ * Auto Mouse自体は無効化しない。
+ */
+#define COMBO_ONLY_FROM_LAYER 0
